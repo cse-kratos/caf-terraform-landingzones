76eb6e2ee247a340fcd03f9098f272bc739dc8bd
Malcolm Miller <malmiller@microsoft.com>
2021-02-25 21:34:35 +0000

    added resources from mlops into landingzone.tf

--- landingzones/caf_solutions/landingzone.tf
+++ landingzones/caf_solutions/landingzone.tf
@@ -1,6 +1,7 @@
 module "caf" {
-  source  = "aztfmod/caf/azurerm"
-  version = "~>5.1.0"
+  source = "../../../modules"
+  # source  = "aztfmod/caf/azurerm"
+  # version = "~>5.1.0"
 
   azuread_api_permissions      = var.azuread_api_permissions
   azuread_apps                 = var.azuread_apps
@@ -55,6 +56,7 @@ module "caf" {
     mysql_servers                     = var.mysql_servers
     postgresql_servers                = var.postgresql_servers
     synapse_workspaces                = var.synapse_workspaces
+    app_config                        = var.app_config
   }
 
   networking = {
@@ -135,5 +137,17 @@ module "caf" {
     app_service_plans            = var.app_service_plans
     app_services                 = var.app_services
     azurerm_application_insights = var.azurerm_application_insights
+    function_apps                = var.function_apps
+  }
+
+  logic_app = {
+    integration_service_environment = var.integration_service_environment
+    logic_app_action_custom         = var.logic_app_action_custom
+    logic_app_action_http           = var.logic_app_action_http
+    logic_app_integration_account   = var.logic_app_integration_account
+    logic_app_trigger_custom        = var.logic_app_trigger_custom
+    logic_app_trigger_http_request  = var.logic_app_trigger_http_request
+    logic_app_trigger_recurrence    = var.logic_app_trigger_recurrence
+    logic_app_workflow              = var.logic_app_workflow
   }
 }
--- landingzones/caf_solutions/variables.tf
+++ landingzones/caf_solutions/variables.tf
@@ -267,3 +267,33 @@ variable dns_zone_records {
 variable keyvault_keys {
   default = {}
 }
+variable integration_service_environment {
+  default = {}
+}
+variable logic_app_action_custom {
+  default = {}
+}
+variable logic_app_action_http {
+  default = {}
+}
+variable logic_app_integration_account {
+  default = {}
+}
+variable logic_app_trigger_custom {
+  default = {}
+}
+variable logic_app_trigger_http_request {
+  default = {}
+}
+variable logic_app_trigger_recurrence {
+  default = {}
+}
+variable logic_app_workflow {
+  default = {}
+}
+variable app_config {
+  default = {}
+}
+variable function_apps {
+  default = {}
+}
